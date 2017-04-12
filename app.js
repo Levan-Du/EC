cfe92fe075d5d729d7b86b5415dcec6c52da54e2
ADu80 <441489115@qq.com>
2017-04-12 12:01:11 +0800

    koa

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const app = new Koa();
 
-app.use(serve('.'));
+app.use(serve('./dist'));
 
-app.listen(80, () => {
+app.listen(3000, () => {
     console.log('websit is running...');
 });
